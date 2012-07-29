9d02028805a756dcc1db259be80f9928bb8f1787
vedanth <vedanth@gmail.com>
2012-07-29 16:24:00 +0530

    c

---
+++ src/main/java/jar/abc/ErrorTranslation.java
@@ -0,0 +1,13 @@
+package net.treetechnologies.common.entities;
+
+import java.io.Serializable;
+
+public class ErrorTranslation implements Serializable
+{
+
+	public String getInternalErrorCode() 
+	{
+		return "Finally................@second";
+	}
+
+}
